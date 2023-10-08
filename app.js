05e7844abcfec4139ad9b5b3f0cdc8bc5a33b196
axelfirdaus <axelfirdaus3005@gmail.com>
2023-10-08 12:03:40 +0700

    version 1 upload

---
+++ app.js
@@ -0,0 +1,96 @@
+const signText = document.querySelector('.player-view')
+const normText = document.querySelector('#player-text')
+
+const textArea = document.querySelector('#text-area')
+
+const msgInput = document.querySelector('.message-input')
+const sendBtn = document.querySelector('#send-btn')
+const container = document.getElementById("container");
+
+msgInput.value = '';
+let letters = [];
+sendBtn.addEventListener('click', function() {
+    textToImage();
+})
+
+document.querySelector('#message-container').addEventListener('keypress', function (e) {
+    console.log(e)
+    if (e.key === 'Enter') {
+        textToImage();
+    }
+});
+
+
+function textToImage(){
+    // textArea.value = ''
+    let text = msgInput.value;
+    msgInput.value = '';
+    container.innerHTML = ''
+    textArea.innerHTML = '';
+    let newText = "";
+
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(function() {
+
+            const players = document.createElement("div");
+            const playerView = document.createElement("div");
+            const playerText = document.createElement("div");
+
+            playerView.textContent = text[i];
+            playerText.textContent = text[i];
+            players.classList.add("players");
+            playerView.classList.add("player-view");
+            playerText.classList.add("player-text");
+        
+            players.appendChild(playerView);
+            players.appendChild(playerText);
+            container.appendChild(players);
+
+            // signText.innerHTML = text[i];
+            // normText.innerHTML = text[i];
+
+            const letter = text.charAt(i);
+            const isLastLetter = (i === text.length - 1);
+
+            if (isLastLetter) {
+                // newText += `<p style="background-color:red;">${letter}</p>`;
+                newText += letter
+
+
+            } else {
+                newText += letter;
+            }
+
+            textArea.innerHTML = newText;
+            // await new Promise(r => setTimeout(r, 1000));
+
+            // if (isLastLetter) {
+            //     const lastLetterElement = textArea.lastChild.lastChild;
+            //     lastLetterElement.classList.add("highlight");
+            // }
+
+
+
+            // const letter = text.charAt(i);
+            // const isLastLetter = (i === text.length - 1);
+
+            // if (isLastLetter) {
+            //     newText += `<span class="highlight">${letter}</span>`;
+            // } else {
+            //     newText += letter;
+            // }
+
+            // textArea.value = newText;
+
+
+
+            // const lastLetter = text.slice(-1);
+            // const lastIndex = text.lastIndexOf(lastLetter);
+            // const newText = text.slice(0, lastIndex) + `<span class="highlight">${text[i]}</span>` + text.slice(lastIndex + 1);
+
+            // textArea.innerHTML = newText;
+
+            console.log(i)
+        }, i * 1000)
+    }
+}
